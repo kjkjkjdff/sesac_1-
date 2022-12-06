e579b06f2f313cd00de5c24082604f769a619f43
mijeongkim3 <yellowredcandy3@gmail.com>
2022-12-06 19:20:12 +0900

    feat: 20221206666

--- nodejs/mvc_mysql_practice answer/controller/Cuser.js
+++ nodejs/mvc_mysql_practice answer/controller/Cuser.js
@@ -20,13 +20,16 @@ exports.post_signin = (req,res) => {
     User.post_signin(req.body.id, req.body.pw, function(result){
         if ( result.length > 0 ) res.send(true);  //이거model의 18번재줄 sql문에서 각각아이디랑 비번에 해당하는 행?이 일치하는게있으면 한줄가져오니까 여기서 result.length가 1이되는거고 그럼 트루임!
         else res.send(false);
+        console.log( result);
+        console.log(result.length);
     });
 }
 
 exports.profile = (req,res) => {
     User.get_user(req.body.id, function(result){
-        if ( result.length > 0 ) res.render("profile", {data: result[0]});
+        if ( result.length > 0 ) res.render("profile", {data: result[0]}); //배열전체
         else res.redirect("/user/signin");
+        console.log(result);
     })
 }
 
