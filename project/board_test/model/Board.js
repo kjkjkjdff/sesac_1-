42ecf0bdb1afbb0528b578b7248c5920df02279f
mijeongkim3 <yellowredcandy3@gmail.com>
2022-12-24 18:30:50 +0900

    202212224

--- project/board_test/model/Board.js
+++ project/board_test/model/Board.js
@@ -1,49 +1,99 @@
+// const Board = ( Sequelize, DataTypes)=>{  
+//     return Sequelize.define(    
+//       "board_list2", 
+//       {
+//           number: { 
+//               type: DataTypes.INTEGER.UNSIGNED,
+//               allowNull: false, 
+//               primaryKey: true, 
+//               autoIncrement: true
+//           },
+
+//           title : { 
+//              type: DataTypes.STRING(200),  
+//              allowNull: false 
+//           },
+
+//            id : { 
+//             type: DataTypes.STRING(20),  
+//             allowNull: false 
+//           },
+//            content : {
+//             type: DataTypes.TEXT,
+//             allowNull: true   
+//            },
+//            date : { 
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//             defaultValue: DataTypes.NOW
+//           },
+
+//            hit: { 
+//               type: DataTypes.INTEGER.UNSIGNED,
+//               allowNull: false,
+//               defaulteValue : "0"
+//           },
+//           filename : { 
+//            type: DataTypes.STRING(255),  
+//            allowNull: true 
+//          },
+//       },
+//       {
+//           tableName: "board_list2",  
+//           freezeTableName: true,
+//           timestamps: false 
+//       }
+//   )
+// }
+
+// module.exports = Board;
+
+
 const Board = ( Sequelize, DataTypes)=>{  
-    return Sequelize.define(    
-      "board_list2", 
+  return Sequelize.define(    
+      "board_list",
       {
           number: { 
-              type: DataTypes.INTEGER.UNSIGNED,
+              type: DataTypes.INTEGER,
               allowNull: false, 
               primaryKey: true, 
               autoIncrement: true
           },
-
           title : { 
-             type: DataTypes.STRING(200),  
-             allowNull: false 
+              type: DataTypes.STRING(200),  
+              allowNull: false 
           },
-
-           id : { 
-            type: DataTypes.STRING(20),  
-            allowNull: false 
+          id : { 
+              type: DataTypes.STRING(20),  
+              allowNull: false 
           },
-           content : {
-            type: DataTypes.TEXT,
-            allowNull: true   
-           },
-           date : { 
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW
+          content : {
+              type: DataTypes.TEXT,
+              allowNull: true   
           },
-
-           hit: { 
+          date : { 
+              type: DataTypes.DATE,
+              allowNull: false,
+              defaultValue: DataTypes.NOW
+          },
+          hit: { 
               type: DataTypes.INTEGER.UNSIGNED,
               allowNull: false,
               defaulteValue : "0"
           },
           filename : { 
-           type: DataTypes.STRING(255),  
-           allowNull: true 
-         },
+              type: DataTypes.STRING(255),  
+              allowNull: true 
+          },
       },
       {
-          tableName: "board_list2",  
+          tableName: "board_list",  
           freezeTableName: true,
           timestamps: false 
       }
-  )
+)
 }
 
 module.exports = Board;
+
+
