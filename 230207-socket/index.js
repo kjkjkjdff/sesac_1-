ec857ed24ee22f818c9de00f5fa47b0f1cd230e4
mijeongkim3 <yellowredcandy3@gmail.com>
2023-02-14 19:21:18 +0900

    feat:202302144444

--- 230207-socket/index.js
+++ 230207-socket/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 //클라이언트 소켓이 연결되면 콜백 함수가 실행된다
-//socket:클라이언트 소켓과 연결되고 새로 생성된 소켓
+//socket:클라이언트 소켓과 연결되고 새로 생성된 소켓.
 io.on('connection', (socket)=> {
   
   console.log('Server Socket Connected', socket.id );
